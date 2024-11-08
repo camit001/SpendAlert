0ad2d227ef0ca70b8531ebc73b531636b9a5c4d3
Amit Kumar <150317204+camit001@users.noreply.github.com>
2024-11-08 22:30:50 +0530

    Add files via upload

---
+++ SpendAlert/Models/SpendAlertDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendAlert.Models
+{
+    public class SpendAlertDbContext :DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendAlertDbContext(DbContextOptions<SpendAlertDbContext> options)
+            :base(options)  
+        {
+            
+        }
+    }
+}
